eb93b7dd62b6be17679a85ac55363ebec361178f
nahidsultan19 <nahidsultan38@gmail.com>
2021-12-06 14:41:38 +0600

    Added new model class Passenger.

--- flights/admin.py
+++ flights/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Airport, Flight
+from .models import Airport, Flight, Passenger
 
 
 # Register your models here.
@@ -11,3 +11,4 @@ class FlightAdmin(admin.ModelAdmin):
 admin.site.register(Airport)
 # admin.site.register(Flight)
 admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger)
